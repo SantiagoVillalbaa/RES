52eeb4fd1b69458ff8966e92d638a446c1745c63
Santiago <santiagovillalbaaa@hotmail.com>
2023-01-16 19:39:13 -0300

    Arreglando fotos y scroll

--- js/productos.js
+++ js/productos.js
@@ -9,87 +9,87 @@ class Producto {
 }
 
 let productoCinco = new Producto(
-  "Lomo",
-  1250,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/l/o/lomo_2.jpg",
+  "Milanesa de Carne",
+  1300,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/i/milanesas-carne.jpg",
   5,
   1
 );
 let productoSeis = new Producto(
-  "Picaña",
-  2600,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/i/pica_a_1.jpg",
+  "Milanesa de Peceto",
+  1800,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/i/milanesas-peceto.jpg",
   6,
   1
 );
 let productoSiete = new Producto(
-  "Paleta",
-  1350,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/a/paleta_2.jpg",
+  "Vacio",
+  2400,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/v/a/vacio_2.jpg",
   7,
   1
 );
 let productoOcho = new Producto(
-  "Peceto",
-  2800,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/e/peceto_5.jpg",
+  "Tapa de Nalga",
+  1390,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/t/a/tapa-de-nalga.jpg",
   8,
   1
 );
 let productoNueve = new Producto(
-  "Matambre",
-  1990,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/a/matambre_2.jpg",
+  "Nalga",
+  1850,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/n/a/nalga_1.jpg",
   9,
   1
 );
 
 let productoDiez = new Producto(
-  "Lomo",
-  1250,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/l/o/lomo_2.jpg",
+  "Colita de Cuadril",
+  2600,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/c/o/colita_4.jpg",
   10,
   1
 );
 let productoOnce = new Producto(
-  "Picaña",
-  2600,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/i/pica_a_1.jpg",
+  "Osobuco",
+  790,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/o/s/osobuco_1.jpg",
   11,
   1
 );
 let productoDoce = new Producto(
-  "Paleta",
-  1350,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/a/paleta_2.jpg",
+  "Riñonada de Cuadril",
+  1850,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/r/i/ri_onada-de-cuadril.jpg",
   12,
   1
 );
 let productoTrece = new Producto(
-  "Peceto",
-  2800,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/e/peceto_5.jpg",
+  "Entraña",
+  2990,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/e/n/entra_a_1.jpg",
   13,
   1
 );
 let productoCatorce = new Producto(
-  "Matambre",
-  1990,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/a/matambre_2.jpg",
+  "Ojo de Bife",
+  2650,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/o/j/ojo-de-bife_1.jpg",
   14,
   1
 );
 let productoQuince = new Producto(
-  "Peceto",
-  2800,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/p/e/peceto_5.jpg",
+  "Rack Ojo de Bife",
+  1990,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/r/a/rack-de-ojo-de-bife.jpg",
   15,
   1
 );
 let productoDieciseis = new Producto(
-  "Matambre",
-  1990,
-  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/m/a/matambre_2.jpg",
+  "T-Bone",
+  2650,
+  "https://www.res.com.ar/media/catalog/product/cache/fa1c797dc73bed0459232d1404671b73/t/-/t-bone.jpg",
   16,
   1
 );
